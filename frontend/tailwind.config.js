c4f0d9d5492c83b4d07b481c479ba10458d6129f
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-09-04 18:59:07 +0800

    complete navbar styles

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'yellow-primary': '#F6E27F',
+    }
+  },
+  plugins: [],
+}
+
