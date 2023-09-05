116b7e1a883486efe7ae359b891075ced42e7485
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-09-05 17:30:30 +0800

    add styles for login input

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { BoardsComponent } from './components/boards/boards.component';
 import { ReportsComponent } from './components/reports/reports.component';
 import { MyProfileComponent } from './components/my-profile/my-profile.component';
 import { PetProfileComponent } from './components/pet-profile/pet-profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,9 @@ import { PetProfileComponent } from './components/pet-profile/pet-profile.compon
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
