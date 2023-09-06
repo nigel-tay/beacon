c8224ca1d80eaa64585c1add2185a2b568482c55
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-09-06 12:32:48 +0800

    add hover effects and button

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       current: 'currentColor',
       'yellow-primary': '#F6E27F',
       'blue-button': '#7AB9F2',
+      'blue-button-hover': '#B1D9FD',
       'pink-accent': '#FD9682',
     }
   },
