83b584ff0f8c60eb31d2c2fa2c7678e2ca7a51a3
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-09-08 14:58:18 +0800

    add maps service

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -20,12 +20,6 @@ export class RegisterComponent implements OnInit{
 
   constructor(private fb: FormBuilder, private placesService: PlacesService) {}
 
-        // // If you want to move your map center to the searched lat long position. Use below any one
-        // // (with animation)
-        // this.gmap.panTo({ lat: place.geometry.location.lat(), lng: place.geometry.location.lng() }); 
-        // // (without animation)
-        // this.gmap.setCenter({ lat: place.geometry.location.lat(), lng: place.geometry.location.lng() }); 
-
   ngOnInit(): void {
     this.initialiseRegisterForm();
     this.placesService.initialisePlacesAutoComplete(this.autocomplete, this.addressInput);
